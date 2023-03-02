fd6f7c62766f7ddd172ef4ea69e179c9c619fc3b
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-02 11:08:53 +0100

    .

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,10 +1,3 @@
-export default async function handler(req: any, res: any) {
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-    return;
-  }
-
-  const { data } = req.body;
-
-  res.json({ message: "Success" });
+export async function GET(request: Request) {
+  return new Response("Hello, Auth");
 }

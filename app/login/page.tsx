9b71a60a13ab6f83d2d0eb8f3eddc96ae4cf20c9
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-02 09:38:59 +0100

    ...

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
 import styles from "./login.module.css";
 
 const Login = () => {
-  return <div className={styles.main}>Login</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <button className={styles.button}>Login with GitHub</button>
+        <button className={styles.button}>Login with Google</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
